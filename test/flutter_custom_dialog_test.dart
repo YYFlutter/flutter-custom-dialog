f319652f498a2343a026df6c6ea3cbdcf1a920a2
wanwusangzhi <yangshangzhi@yy.com>
2019-08-20 20:15:06 +0800

    modify: update yarm file

--- test/flutter_custom_dialog_test.dart
+++ test/flutter_custom_dialog_test.dart
@@ -1,21 +1,2 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_custom_dialog/flutter_custom_dialog.dart';
-
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_custom_dialog');
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await FlutterCustomDialog.platformVersion, '42');
-  });
 }
