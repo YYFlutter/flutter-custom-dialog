6e5a197283b82f35de03cae9435b938dd5988ad2
yangshangzhi <yangshangzhi@yy.com>
2019-09-30 18:17:28 +0800

    feat: 增加context处理

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Program Files\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\YPlugin\package\flutter_custom_dialog\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Program Files\flutter\bin\cache\artifacts\engine\ios"
