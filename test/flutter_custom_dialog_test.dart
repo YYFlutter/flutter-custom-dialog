bfd2544d7db7fb16aa2988d5913414fcf679102f
yangshangzhi <yangshangzhi@yy.com>
2019-10-09 19:29:44 +0800

    remove test

--- test/flutter_custom_dialog_test.dart
+++ test/flutter_custom_dialog_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_custom_dialog/flutter_custom_dialog.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_custom_dialog');
@@ -15,7 +14,4 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterCustomDialog.platformVersion, '42');
-  });
 }
