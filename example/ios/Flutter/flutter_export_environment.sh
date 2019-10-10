7a96b32cc93b83ed9483054f82d3b3cb24e75d03
xuyingjun <xuyingjun@yy.com>
2019-10-10 15:55:32 +0800

    Revert: refactor: update project

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/wanwu/program/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wanwu/Desktop/code/yy/test/flutter_custom_dialog/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\FlutterSDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\YYLive4-OpenSource\flutter-custom-dialog\example"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/wanwu/program/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
--- ios/Classes/FlutterCustomDialogPlugin.m
+++ ios/Classes/FlutterCustomDialogPlugin.m
@@ -1,8 +1,20 @@
 #import "FlutterCustomDialogPlugin.h"
-#import <flutter_custom_dialog/flutter_custom_dialog-Swift.h>
 
 @implementation FlutterCustomDialogPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterCustomDialogPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_custom_dialog"
+            binaryMessenger:[registrar messenger]];
+  FlutterCustomDialogPlugin* instance = [[FlutterCustomDialogPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
