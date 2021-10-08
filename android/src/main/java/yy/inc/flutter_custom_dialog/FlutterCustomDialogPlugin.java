98c31dfbba75404048286d81e3b5e8f6f4bb044d
CyJaySong <cyjaysong@cqgynet.com>
2021-10-08 17:16:50 +0800

    migrating the plugin to the V2 embedding

--- android/src/main/java/yy/inc/flutter_custom_dialog/FlutterCustomDialogPlugin.java
+++ android/src/main/java/yy/inc/flutter_custom_dialog/FlutterCustomDialogPlugin.java
@@ -9,7 +9,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** UntitledPlugin */
-public class UntitledPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterCustomDialogPlugin implements FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
