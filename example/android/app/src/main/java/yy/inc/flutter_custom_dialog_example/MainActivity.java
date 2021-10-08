0ab8e0a2a88ade4e3b9b45dbd4f6ede121fb9992
CyJaySong <cyjaysong@cqgynet.com>
2021-10-08 17:07:09 +0800

    migrating the plugin to the V2 embedding

--- example/android/app/src/main/java/yy/inc/flutter_custom_dialog_example/MainActivity.java
+++ example/android/app/src/main/java/yy/inc/flutter_custom_dialog_example/MainActivity.java
@@ -1,13 +1,6 @@
 package yy.inc.flutter_custom_dialog_example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
